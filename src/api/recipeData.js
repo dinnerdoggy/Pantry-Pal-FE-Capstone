3a8b1309917cb1f0ad78f6c7320382700482a8be
Casey <dinnerdoggy@gmail.com>
2024-11-19 21:31:39 -0600

    Did some file structuring to prepare for Create and view details on recipes

--- src/api/recipeData.js
+++ src/api/recipeData.js
@@ -16,6 +16,21 @@ const getRecipes = (userId) =>
       .catch(reject);
   });
 
+// CREATE Recipe
+const createRecipe = (payload) =>
+  new Promise((resolve, reject) => {
+    fetch(`${endpoint}/recipe.json`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(payload),
+    })
+      .then((response) => response.json())
+      .then((data) => resolve(data))
+      .catch(reject);
+  });
+
 // DELETE Recipe
 const deleteRecipe = (firebaseKey) =>
   new Promise((resolve, reject) => {
@@ -30,4 +45,4 @@ const deleteRecipe = (firebaseKey) =>
       .catch(reject);
   });
 
-export { getRecipes, deleteRecipe };
+export { getRecipes, createRecipe, deleteRecipe };
