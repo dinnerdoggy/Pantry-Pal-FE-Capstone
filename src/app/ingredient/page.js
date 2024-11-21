ee68d51df0a0b2873a0a57f1cc8d35305ffd8edd
Casey <dinnerdoggy@gmail.com>
2024-11-20 21:52:12 -0600

    optimized speed by switching navbar links to employ useRouter instead

---
+++ src/app/ingredient/page.js
@@ -0,0 +1,7 @@
+'use client';
+
+import React from 'react';
+
+export default function IngredientPage() {
+  return <div>Ingredient Page</div>;
+}
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,22 +1,37 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
-import Link from 'next/link';
 import { Navbar, Container, Nav, Button } from 'react-bootstrap';
+import { useRouter } from 'next/navigation';
 import { signOut } from '../utils/auth';
 
 export default function NavBar() {
+  const router = useRouter();
+
+  // Reusable html tag properties to keep things dry
+  const createNavLinkProps = (route) => ({
+    onClick: () => router.push(route), // onClick event that routes
+    onKeyDown: (event) => {
+      // linter needed a this.onKeyDown for use with an onClick event in a non-interactable html tag, but "this" doesn't exist in a stateless page. This is a workaround
+      if (event.key === 'Enter' || event.key === ' ') {
+        router.push(route);
+      }
+    },
+    role: 'link', // linter also needed a role and tabIndex for using an onClick event with a non-interactable html tag
+    tabIndex: 0,
+    className: 'nav-link',
+  });
+
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
-        <Link passHref href="/" className="navbar-brand">
+        <a {...createNavLinkProps('/')} className="navbar-brand">
           Pantry Pal
-        </Link>
+        </a>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <Link className="nav-link" href="/recipe/new">
-              Add a Recipe
-            </Link>
+            <a {...createNavLinkProps('/recipe/new')}>Add a Recipe</a>
+            <a {...createNavLinkProps('/ingredient')}>Ingredients</a>
           </Nav>
 
           <Button variant="danger" onClick={signOut}>
